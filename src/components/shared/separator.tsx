1504bcbb978e4a4fe0cccad290ca906b62e49903
Franx0 <francisco.moya.martinez@gmail.com>
2023-04-23 21:14:43 +0200

    Stop Mad Cool

---
+++ src/components/shared/separator.tsx
@@ -0,0 +1,3 @@
+export default function Separator() {
+  return <hr className="w-full h-px mb-10 bg-gray-200" />
+}
---
+++ src/languages/es.ts
@@ -0,0 +1,19 @@
+export default {
+  header: `Los vecinos de Villaverde y Getafe crean la plataforma vecinal Stop Espacio Mad Cool en rechazo a este nuevo espacio destinado a macrofestivales en una localización muy cercana a los barrios y viviendas de Marconi y Getafe Norte.`,
+  pressRelease: {
+    description: `Si quieres saber más acerca de lo que desde la plataforma reivindicamos, puedes leer la nota de prensa que hemos preparado para conocer toda la información acerca de este atropello al descanso, bienestar y salud de los barrios del sur de Madrid.`,
+    download: `Para leer la nota de prensa puedes seguir el siguiente enlace de descarga:&nbsp;<a href='#' hrefLang='es' referrerPolicy='origin' rel='author' target='_blank' title='Nota de prensa'>Nota de prensa</a>`
+  },
+  location: `En el siguiente plano de situación se observa la localización exacta del recinto Espacio Mad Cool (marcada en color morado) y su cercanía a los barrios de Marconi en Villaverde y Getafe Norte en Getafe. Podeis leer más <a class='text-blue-400' href="https://drive.google.com/uc?export=view&id=1dTNZxDN75kyjlsY8CAFzPV0vT9juJACQ" target='_blank'>aquí</a> sobre como este nuevo recinto va a afectar a los barrios limítrofes y sus vecinos.`,
+  news: {
+    title: `Noticias relacionadas:`,
+    links: [
+      {
+        date: `2 de Marzo de 2023`,
+        name: `Dueños de Mad Cool compran suelo en Villaverde`,
+        alt: `servimedia`,
+        url: `https://www.servimedia.es/noticias/duenos-mad-cool-compran-suelo-villaverde-madrid-para-construir-gran-ciudad-musica/3634230`
+      }
+    ],
+  }
+}
