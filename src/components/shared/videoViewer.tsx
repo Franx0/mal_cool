9bfed8eef592740d3e71c8f8331bb8be9e66219a
Franx0 <francisco.moya.martinez@gmail.com>
2024-07-14 14:52:19 +0200

    Fix video iframe aspect ratio

--- src/components/shared/videoViewer.tsx
+++ src/components/shared/videoViewer.tsx
@@ -6,6 +6,6 @@ type Props = {
 
 export default function VideoViewer(props: Props) {
   return (
-    <iframe className={props.className} width="100%" height="315" src={props.url} title={props.text} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    <iframe className={`aspect-square sm:aspect-video ${props.className}`} width="560" height="315" src={props.url} title={props.text} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
   )
 };
